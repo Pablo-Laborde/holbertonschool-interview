b3527b8bb613a4b94c98904555178c23860f4176
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-01-01 23:34:12 -0300

    ex 0

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,21 +3,21 @@
 
 
 def canUnlockAll(boxes):
-  length = len(boxes)
-  rv = True
-  dic = {0: True}
-  for i in range(length):
-    if i not in dic:
-      dic[i] = False
-  unlockBox(dic, boxes, 0)
-  for each in dic:
-    if dic[each] is False:
-      rv = False
-  return (rv)
+    length = len(boxes)
+    rv = True
+    dic = {0: True}
+    for i in range(length):
+        if i not in dic:
+            dic[i] = False
+    unlockBox(dic, boxes, 0)
+    for each in dic:
+        if dic[each] is False:
+            rv = False
+    return (rv)
 
 
 def unlockBox(dic, boxes, pos):
-  for each in boxes[pos]:
-    if (each in dic) and (dic[each] is False):
-      dic[each] = True
-      unlockBox(dic, boxes, each)
+    for each in boxes[pos]:
+        if (each in dic) and (dic[each] is False):
+            dic[each] = True
+            unlockBox(dic, boxes, each)
