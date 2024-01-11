77e93b643753cfa4ff55cab20d9a9f80ea3092dd
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-01-10 23:23:28 -0300

    ex 0

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -1,21 +1,22 @@
+#include <stdlib.h>
 #include "lists.h"
 
 
 listint_t *insert_node(listint_t **head, int number) {
-	listint_t *nn = NULL, aux = NULL;
+	listint_t *nn = NULL, *aux = NULL;
 
-	nn = malloc(sizeof(listint_s));
+	nn = malloc(sizeof(listint_t));
 	if (nn) {
 		nn->n = number;
 		nn->next = NULL;
 		if (head) {
 			aux = *head;
-			if (!aux || (aux->nn > number)) {
+			if (!aux || (aux->n > number)) {
 					nn->next = aux;
 					*head = nn;
 			}
 			else {
-				while (aux->next && (aux->next < number))
+				while (aux->next && (aux->next->n < number))
 					aux = aux->next;
 				if (!aux->next)
 					aux->next = nn;
