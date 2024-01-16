0e0e90c77749c368b6ab2dedc503965ce435e168
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-01-15 22:23:37 -0300

    0

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -23,9 +23,5 @@ typedef struct binary_tree_s binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
-void binary_tree_print(const binary_tree_t *tree);
-static size_t _height(const binary_tree_t *tree);
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-
 
 #endif
