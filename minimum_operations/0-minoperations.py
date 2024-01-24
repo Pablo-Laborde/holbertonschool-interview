af5714a0e41343b0da6d26b263bfe2ecd77ce92e
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-01-23 23:53:50 -0300

    doc

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,26 +3,26 @@
 
 
 def minOperations(n):
-	""" function """
-	import math
-	if (n > 1):
-		sr = int(math.sqrt(n))
-		mlo = sr
-		while (n % mlo):
-			mlo -= 1
-		mls = int(n / mlo)
-		if (mlo == 1):
-			return (mls)
-		else:
-			axo = minOperations(mlo)
-			axs = minOperations(mls)
-			if ((mlo + axs) < (mls + axo)):
-				return (mlo + axs)
-			else:
-				return (mls + axo)
-	else:
-		return (0)
+    """ function """
+    import math
+    if (n > 1):
+        sr = int(math.sqrt(n))
+        mlo = sr
+        while (n % mlo):
+            mlo -= 1
+        mls = int(n / mlo)
+        if (mlo == 1):
+            return (mls)
+        else:
+            axo = minOperations(mlo)
+            axs = minOperations(mls)
+            if ((mlo + axs) < (mls + axo)):
+                return (mlo + axs)
+            else:
+                return (mls + axo)
+    else:
+        return (0)
 
 
 if __name__ == "__main__":
-	print(minOperations(240))
+    print(minOperations(240))
