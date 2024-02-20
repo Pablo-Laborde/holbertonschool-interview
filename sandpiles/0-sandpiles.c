f11d0e14c1ef0eb3059fc1debc0f8eb590a70f58
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-02-19 22:27:00 -0300

    0

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,21 +1,5 @@
 #include "sandpiles.h"
 
-int main(void)
-{
-	int grid1[3][3] = {
-        {3, 3, 3},
-        {3, 3, 3},
-        {3, 3, 3}
-    };
-    int grid2[3][3] = {
-        {1, 3, 1},
-        {3, 3, 3},
-        {1, 3, 1}
-    };
-	sandpiles_sum(grid1, grid2);
-	return (0);
-}
-
 
 /**
 * sandpiles_sum- func
