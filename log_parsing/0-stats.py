35259449dd6c2072b72c7d1481d22ed0dada0e3f
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-02-21 01:08:46 -0300

    0

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -10,26 +10,28 @@ scdic = {**{key: 0 for key in sc}}
 fsdic = {'fs': 0}
 
 def sigint_func(sig, frame):
-	""" function when ctrl + c """
-	print(f"File size: {fsdic['fs']}")
-	for key, value in (sorted(scdic.items(), key=lambda item: item[1], reverse=True)):
-		if (value):
-			print(f"{key}: {value}")
-	exit(0)
+    """ function when ctrl + c """
+    print(f"File size: {fsdic['fs']}")
+    for key, value in (sorted(scdic.items(),\
+        key=lambda item: item[1], reverse=True)):
+        if (value):
+            print(f"{key}: {value}")
+    exit(0)
 
 
 signal.signal(signal.SIGINT, sigint_func)
 counter = 0
 while (True):
-	line = input()
-	ls = line.split()
-	fsdic['fs'] += int(ls[8])
-	key = int(ls[7])
-	scdic[key] += 1
-	counter += 1
-	if (counter == 10):
-		counter = 0
-		print(f"File size: {fsdic['fs']}")
-		for key, value in (sorted(scdic.items(), key=lambda item: item[1], reverse=True)):
-			if (value):
-				print(f"{key}: {value}")
+    line = input()
+    ls = line.split()
+    fsdic['fs'] += int(ls[8])
+    key = int(ls[7])
+    scdic[key] += 1
+    counter += 1
+    if (counter == 10):
+        counter = 0
+        print(f"File size: {fsdic['fs']}")
+        for key, value in (sorted(scdic.items(),\
+            key=lambda item: item[1], reverse=True)):
+            if (value):
+                print(f"{key}: {value}")
