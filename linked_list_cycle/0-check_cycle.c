d0646edc0eb3a32f24b01ebb2fa8575962a5d7dd
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-02-22 17:59:46 -0300

    0

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -8,13 +8,21 @@
 */
 int check_cycle(listint_t *list)
 {
-	listint_t* snake_head = list, snake_tail = list;
+	int adv = 0;
+	listint_t* snake_head = list, *snake_tail = list;
 
 	while (snake_head)
 	{
 		snake_head = snake_head->next;
 		if (snake_head == snake_tail)
 			return (1);
+		if (adv)
+		{
+			snake_tail = snake_tail->next;
+			adv = 0;
+		}
+		else
+			adv = 1;
 	}
 	return (0);
 }
