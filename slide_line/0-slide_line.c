49d42bcd1158db7f6c674e43dd81c75d7c3c0118
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-03-22 22:46:20 -0300

    0

---
+++ slide_line/0-slide_line.c
@@ -0,0 +1,100 @@
+#include "slide_line.h"
+
+
+/**
+* slide_line- func
+* @line: int *
+* @size: size_t
+* @direction: int
+* Return: int
+*/
+int slide_line(int *line, size_t size, int direction)
+{
+	if (direction == SLIDE_LEFT)
+		sl(line, size);
+	else if (direction == SLIDE_RIGHT)
+		sr(line, size);
+	else
+		return (0);
+	return (1);
+}
+
+
+/**
+* sl- func
+* @line: int *
+* @size: size_t
+* Return: void
+*/
+void sl(int *line, size_t size)
+{
+	size_t pr = 0, a = 1;
+
+	while (a < size)
+	{
+		while ((line[a] == 0) && (a < size - 1))
+			a++;
+		if (line[a])
+		{
+			if (pr != a)
+				if (line[pr])
+					if (line[pr] != line[a])
+						pr++;
+					else
+					{
+						line[pr] += line[a];
+						line[a] = 0;
+						pr++;
+						a++;
+					}
+				else
+				{
+					line[pr] += line[a];
+					line[a] = 0;
+					a++;
+				}
+			else
+				a++;
+		}
+	}
+}
+
+
+/**
+* slider- func
+* @line: int *
+* @size: size_t
+* Return: void
+*/
+void sr(int *line, size_t size)
+{
+	size_t pr = size - 1, a = size - 2;
+
+	while (a < size)
+	{
+		while ((line[a] == 0) && (a < size))
+			a--;
+		if (line[a])
+		{
+			if (pr != a)
+				if (line[pr])
+					if (line[pr] != line[a])
+						pr--;
+					else
+					{
+						line[pr] += line[a];
+						line[a] = 0;
+						pr--;
+						a--;
+					}
+				else
+				{
+					line[pr] += line[a];
+					line[a] = 0;
+					a--;
+				}
+			else
+				a--;
+		}
+	}
+}
