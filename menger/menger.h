b69d4d2af9311341665d16237145b396829c93e2
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-03-28 00:51:09 -0300

    0

---
+++ menger/menger.h
@@ -0,0 +1,15 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+
+/* Libraries */
+	#include <stdio.h>
+
+
+/* Functions */
+	void menger(int level);
+	int valid(int level, int i, int j);
+	int nsize(int level);
+
+
+#endif
