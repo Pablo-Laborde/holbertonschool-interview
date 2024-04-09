7b1b6993e49317cb05bd928e2c231bb42ae6a429
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-04-09 00:24:11 -0300

    0

---
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,52 @@
+#include "binary_trees.h"
+
+
+/**
+* sorted_array_to_avl- func
+* @array: int *
+* @size: size_t
+* Return: avl_t *
+*/
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	avl_t *root = NULL;
+
+	root = make_avl(array, NULL, 0, size - 1);
+	return (root);
+}
+
+
+/**
+* make_avl- func
+* @array: int *
+* @parent: avl_t *
+* @b: size_t
+* @e: size_t
+* Return: avl_t *
+*/
+avl_t *make_avl(int *array, avl_t *parent, size_t b, size_t e)
+{
+	size_t mid = 0;
+	avl_t *node = NULL;
+
+	if (e < b)
+		return (NULL);
+	mid = ((e - b) % 2) ? ((e - b) / 2 + 1) : ((e - b) / 2);
+	mid += b;
+	node = malloc(sizeof(avl_t));
+	if (!node)
+		exit(1);
+	node->n = array[mid];
+	node->parent = parent;
+	if (b == e)
+	{
+		node->left = NULL;
+		node->right = NULL;
+	}
+	else
+	{
+		node->left = make_avl(array, node, b, mid - 1);
+		node->right = make_avl(array, node, mid + 1, e);
+	}
+	return (node);
+}
---
+++ sorted_array_to_avl/binary_trees.h
@@ -0,0 +1,41 @@
+#ifndef BT_H
+#define BT_H
+
+/* Libraries */
+	#include <stdio.h>
+	#include <stdlib.h>
+#include <string.h>
+
+
+/* Structures */
+	/**
+	* struct binary_tree_s - Binary tree node
+	*
+	* @n: Integer stored in the node
+	* @parent: Pointer to the parent node
+	* @left: Pointer to the left child node
+	* @right: Pointer to the right child node
+	*/
+	struct binary_tree_s
+	{
+			int n;
+			struct binary_tree_s *parent;
+			struct binary_tree_s *left;
+			struct binary_tree_s *right;
+	};
+	typedef struct binary_tree_s binary_tree_t;
+	typedef struct binary_tree_s avl_t;
+
+
+/* Functions */
+	/* 0-sorted_array_to_avl.c */
+	avl_t *sorted_array_to_avl(int *array, size_t size);
+	avl_t *make_avl(int *array, avl_t *parent, size_t b, size_t e);
+
+
+	/* print_tree.c */
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+		static size_t _height(const binary_tree_t *tree);
+		void binary_tree_print(const binary_tree_t *tree);
+
+#endif
