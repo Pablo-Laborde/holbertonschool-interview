5e134ae4ec26ac46c919caa03fa652b34f21f8e7
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-05-12 04:19:06 -0300

    0

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -9,34 +9,53 @@
 */
 void heap_sort(int *array, size_t size)
 {
-	size_t p1 = size, p2 = size, end = 0, mid = 0;
-	int change = 0, a = 0;
+	sol_one(array, size);
+}
+
+
+/**
+* swap- func
+* @array: int *
+* @size: size_t
+* @p1: size_t
+* @p2: size_t
+* Return: void
+*/
+void swap(int *array, size_t size, size_t end, size_t beg)
+{
+	int a = 0;
+
+	a = array[end];
+	array[end] = array[beg];
+	array[beg] = a;
+	print_array(array, size);
+}
+
+
+/**
+* sol_one- func
+* @array: int *
+* @size: size_t
+* Return: void
+*/
+void sol_one(int *array, size_t size)
+{
+	size_t olp = size, ulp = 0, end = 0, mid = 0;
 
 	do {
-		p2 = p1;
-		change = 0;
+		ulp = olp;
 		do {
-			end = p2;
+			end = ulp;
 			mid = end / 2;
 			while ((mid > 0) && (array[end - 1] > array[mid - 1]))
 			{
-				a = array[end - 1];
-				array[end - 1] = array[mid - 1];
-				array[mid - 1] = a;
-				change = 1;
+				swap(array, size, end - 1, mid - 1);
 				end /= 2;
 				mid = end / 2;
-				print_array(array, size);
 			}
-			p2--;
-		} while (p2);
-		if (change)
-		{
-			a = array[0];
-			array[0] = array[p1 - 1];
-			array[p1 - 1] = a;
-			print_array(array, size);
-			p1--;
-		}
-	} while (change);
+			ulp--;
+		} while (ulp);
+		swap(array, size, 0, olp - 1);
+		olp--;
+	} while (olp > 1);
 }
--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -8,6 +8,10 @@
 
 /* Functions */
 	void print_array(const int *array, size_t size);
+
 	void heap_sort(int *array, size_t size);
+	void swap(int *array, size_t size, size_t p1, size_t p2);
+
+	void sol_one(int *array, size_t size);
 
 #endif
