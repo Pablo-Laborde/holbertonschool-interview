8226c5c89af2fdaf1bc6342c7beb2d217dcc1340
Pablo <p5.tacoronte@gmail.com>
2024-07-13 17:00:39 +0000

    0

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -5,16 +5,15 @@
 def rain(walls):
     """Rain function."""
     print(walls)
-    l = len(walls)
+    le = len(walls)
     volume = 0
     w1 = 0
-    while ((w1 < l) and (walls[w1] == 0)):
+    while ((w1 < le) and (walls[w1] == 0)):
         w1 += 1
     w2 = w1 + 1
-    while (w2 < l):
-        while ((w2 < (l - 1)) and (walls[w2] == 0)):
+    while (w2 < le):
+        while ((w2 < (le - 1)) and (walls[w2] == 0)):
             w2 += 1
-        print(f"{w1} -> {w2}")
         if (walls[w1] > walls[w2]):
             h = walls[w2]
         else:
