4ae8613a7f115449dbedee721af75eee46dfec38
Pablo <p5.tacoronte@gmail.com>
2024-07-14 03:59:05 +0000

    0

---
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Exercise 0"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotate the matrix."""
+    length = len(matrix)
+    for i in range(length):
+        for j in range(i):
+            a = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = a
+    for i in range(length):
+        for j in range((length - 1) // 2):
+            a = matrix[i][j]
+            matrix[i][j] = matrix[i][length - 1 - j]
+            matrix[i][length - 1 - j] = a
