a32d49cb7463d7bef528440b0da8c18192986a85
Pablo <p5.tacoronte@gmail.com>
2024-08-16 14:55:45 +0000

    0

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -18,7 +18,7 @@ int wildcmp(char *s1, char *s2)
 		if (s1[0] == '\0')
 			return (0);
 		a1 = wildcmp(s1, s2 + 1);
-		return ((!a1) ? (wildcmp(s1 + 1, s2)): 1);
+		return ((!a1) ? (wildcmp(s1 + 1, s2)) : 1);
 	}
 	if ((s1[0] == '\0') && (s2[0] == '\0'))
 		return (1);
