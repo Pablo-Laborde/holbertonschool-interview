37d50ecc60681d3650fddf465301be8c2c7bcda0
Pablo <p5.tacoronte@gmail.com>
2024-09-12 12:53:47 +0000

    0

--- primegame/main.py
+++ primegame/main.py
@@ -12,3 +12,7 @@ print("Winner: {}".format(isWinner(0, [])))
 print("Winner: {}".format(isWinner(7, [2, 5, 1, 4, 3, 2, 2])))
 
 print("Winner: {}".format(isWinner(-1, [2, 2, 2])))
+
+
+lista = [x for x in range(2, 10002)]
+print("Winner: {}".format(isWinner(10000, lista)))
